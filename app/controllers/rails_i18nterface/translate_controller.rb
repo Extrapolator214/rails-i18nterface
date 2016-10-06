a84c7736daf0394e8248bc97027f88b188725545
Grigory Reznichenko <gr.reznichenko@gmail.com>
2016-10-06 19:32:55 +0300

    permit tranlsate params

--- app/controllers/rails_i18nterface/translate_controller.rb
+++ app/controllers/rails_i18nterface/translate_controller.rb
@@ -8,7 +8,7 @@ module RailsI18nterface
 
     def index
       @keys = RailsI18nterface::Keys.new(Rails.root, @from_locale, @to_locale)
-      @keys.apply_filters(params)
+      @keys.apply_filters(translation_params)
       @keys.paginate(@page, @per_page)
       @total_entries = @keys.all_keys.size
       @page_title = 'Translate'
@@ -16,12 +16,12 @@ module RailsI18nterface
     end
 
     def destroy
-      params[:key] = { params[:del] => '' }
+      translation_params[:key] = { translation_params[:del] => '' }
       update
     end
 
     def export
-      locale = params[:locale].to_sym
+      locale = translation_params[:locale].to_sym
       keys = { locale => I18n.backend.send(:translations)[locale] || {} }
       remove_blanks keys
       yaml = keys_to_yaml(keys)
@@ -31,20 +31,20 @@ module RailsI18nterface
 
     def update
       if I18n.backend.respond_to? :store_translations
-        I18n.backend.store_translations(@to_locale, to_deep_hash(params[:key]))
+        I18n.backend.store_translations(@to_locale, to_deep_hash(translation_params[:key]))
       end
       yaml = RailsI18nterface::Yamlfile.new(Rails.root, @to_locale)
       yaml.write_to_file
       force_init_translations
       flash[:notice] = 'Translations stored'
       reload
-      # redirect_to root_path(params.slice(:filter, :sort_by, :key_type, :key_pattern, :text_type, :text_pattern))
+      # redirect_to root_path(translation_params.slice(:filter, :sort_by, :key_type, :key_pattern, :text_type, :text_pattern))
     end
 
     def reload
       @keys = RailsI18nterface::Keys.new(Rails.root, @from_locale, @to_locale)
       @keys.reload(Rails.root)
-      redirect_to root_path(params.slice(:filter, :sort_by, :key_type, :key_pattern, :text_type, :text_pattern))
+      redirect_to root_path(translation_params.slice(:filter, :sort_by, :key_type, :key_pattern, :text_type, :text_pattern))
     end
 
     protected
@@ -57,23 +57,23 @@ module RailsI18nterface
     end
 
     def init_sort
-      params[:sort_by] ||= 'key'
+      translation_params[:sort_by] ||= 'key'
     end
 
     def init_assigns
       @from_locale = session[:from_locale].to_sym
       @to_locale = session[:to_locale].to_sym
       @per_page = session[:per_page]
-      @page = (params[:page] || 1).to_i
+      @page = (translation_params[:page] || 1).to_i
     end
 
     def init_session
       session[:from_locale] ||= I18n.default_locale
       session[:to_locale] ||= I18n.default_locale
       session[:per_page] ||= 50
-      session[:from_locale] = params[:from_locale] if params[:from_locale].present?
-      session[:to_locale] = params[:to_locale] if params[:to_locale].present?
-      session[:per_page] = params[:per_page].to_i if params[:per_page].present?
+      session[:from_locale] = translation_params[:from_locale] if translation_params[:from_locale].present?
+      session[:to_locale] = translation_params[:to_locale] if translation_params[:to_locale].present?
+      session[:per_page] = translation_params[:per_page].to_i if translation_params[:per_page].present?
     end
 
     def init_translations
@@ -82,6 +82,10 @@ module RailsI18nterface
 
     def force_init_translations
       I18n.backend.send(:init_translations) rescue false
+    end
+    
+    def translation_params
+      params.permit!
     end
 
   end
